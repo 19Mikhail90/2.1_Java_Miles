8d705c3caa3e36472efcb225b5dc64689fa37f94
Mikhail Gozhenko <sacred9900@gmail.com>
2021-08-14 14:51:31 +0900

    First

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 316;
+        int priceBonusMile = 20;
+        int amountMile = ticketPrice / priceBonusMile;
+        System.out.println("Вам начисленно " + amountMile + " миль");
+
+    }
+}
