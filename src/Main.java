b47bd1306460ccbd6acd4ef84d8ee67146b154b2
Mikhail Gozhenko <sacred9900@gmail.com>
2021-08-14 15:33:34 +0900

    changed main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketPrice = 316;
+        int ticketPrice = 20;
         int priceBonusMile = 20;
         int amountMile = ticketPrice / priceBonusMile;
-        System.out.println("Вам начисленно " + amountMile + " миль");
 
+        if (ticketPrice >= 20) {
+            System.out.println("Вам начисленна " + amountMile + " миль");
+        }else {
+            System.out.println("Трать больше!\nТебе нужны эти мили!!!");
+        }
     }
 }
